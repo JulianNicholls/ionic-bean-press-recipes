bb24749d9df0857594d149149677b151bcb9f4b5
Julian Nicholls <juliannicholls29@gmail.com>
2018-01-25 12:35:10 +0000

    Add logo and Aeropress button

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AeropressPage } from '../aeropress/aeropress';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  constructor(public navCtrl: NavController) {}
 
-  constructor(public navCtrl: NavController) {
-
+  showAeropressRecipes() {
+    this.navCtrl.push(AeropressPage);
   }
-
 }
